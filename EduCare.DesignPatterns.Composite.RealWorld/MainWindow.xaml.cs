23dea5f78f9d52594f2d69661999814525684037
Can PERK <canperk@outlook.com>
2017-01-03 01:29:01 +0300

    Composite Pattern

---
+++ EduCare.DesignPatterns.Composite.RealWorld/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using EduCare.DesignPatterns.Composite.RealWorld.Models;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System;
+using Dapper;
+
+namespace EduCare.DesignPatterns.Composite.RealWorld
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var str = ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
+            Connection = new SqlConnection(str);
+            Connection.Open();
+            LoadFirstItem();
+        }
+
+        private void LoadFirstItem()
+        {
+            var emp = Connection.Query<Employee>("SELECT TOP 1 Id, FullName FROM Employees WHERE DirectReports IS NULL").Single();
+            trv.Items.Add(emp);
+        }
+
+        public SqlConnection Connection { get; set; }
+
+        private void trv_Expanded(object sender, RoutedEventArgs e)
+        {
+            var emp = (e.OriginalSource as TreeViewItem).DataContext as Employee;
+            if (emp == null)
+                return;
+            if (emp.Employees.Any())
+                return;
+
+            var employees = Connection.Query<Employee>($"SELECT Id, FullName FROM Employees WHERE DirectReports = {emp.Id}").ToList();
+            employees.ForEach(i => {
+                emp.AddSubEmployee(i);
+            });
+        }
+    }
+}
---
+++ EduCare.DesignPatterns.Composite.RealWorld/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace EduCare.DesignPatterns.Composite.RealWorld.Models
+{
+    public class Employee
+    {
+        public Employee()
+        {
+            Employees = new ObservableCollection<Employee>();
+        }
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Path { get; set; }
+        public ObservableCollection<Employee> Employees { get; set; }
+        public void AddSubEmployee(Employee employee)
+        {
+            Employees.Add(employee);
+        }
+    }
+}
